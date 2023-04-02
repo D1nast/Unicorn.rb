d7708a404da0f965497cd279a064cbeb6b3ebffe
石原 <108358234+D1nast@users.noreply.github.com>
2023-04-02 17:40:56 +0900

    Update unicorn.rb

--- unicorn.rb
+++ unicorn.rb
@@ -1,10 +1,16 @@
 # rootパスのディレクトリを指定
 root_path = File.expand_path('../../', __FILE__)
-# root_path = "/var/rails/APIsnowresort"
+# root_pathは自分のアプリケーションが設置されているディレクトリの絶対パスを返す
+
 # アプリケーションサーバの性能を決定する
+# 通常はCPUのコア数以下に設定する。CPUのコア数より多い数のプロセスを動作させても意味なし
+# 例えば、EC2のt2.microの場合はコア数は1
 worker_processes 2
+
+
 # アプリケーションの設置されているディレクトリを指定
 working_directory root_path
+
 # プロセスIDの保存先を指定
 pid "#{root_path}/tmp/pids/unicorn.pid"
 # ポート番号を指定
@@ -15,6 +21,7 @@ stderr_path "#{root_path}/log/unicorn.stderr.log"
 stdout_path "#{root_path}/log/unicorn.stdout.log"
 #応答時間を待つ上限時間を設定
 timeout 2000
+
 # ダウンタイムなしでUnicornを再起動時する
 preload_app true
 
@@ -46,52 +53,3 @@ end
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# $worker  = 2
-# $timeout = 30
-# $app_dir = "/var/rails/APIsnowresort"
-# $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-# $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-# $std_log = File.expand_path 'log/unicorn.log', $app_dir
-# # set config
-# worker_processes  $worker
-# working_directory $app_dir
-# stderr_path $std_log
-# stdout_path $std_log
-# timeout $timeout
-# listen  $listen
-# pid $pid
-# # loading booster
-# preload_app true
-# # before starting processes
-# before_fork do |server, worker|
-#   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-#   old_pid = "#{server.config[:pid]}.oldbin"
-#   if old_pid != server.pid
-#     begin
-#       Process.kill "QUIT", File.read(old_pid).to_i
-#     rescue Errno::ENOENT, Errno::ESRCH
-#     end
-#   end
-# end
-# # after finishing processes
-# after_fork do |server, worker|
-#   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-# end
